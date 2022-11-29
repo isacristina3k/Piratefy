95cbbc1ccc8410d761f248f0bb4e83ae29ac76a8
isacristina3k <isadoranovaes1@gmail.com>
2022-11-29 09:50:59 -0300

    mudanca do bd

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -5,7 +5,7 @@
     $timezone = date_default_timezone_set("America/Sao_Paulo");
     //https://www.php.net/manual/en/timezones.america.php
 
-    $con = mysqli_connect("aws-piratefy-db.ckcmolkh760v.us-east-1.rds.amazonaws.com", "admin", "admin12345", "piratefy");
+    $con = mysqli_connect("db-piratefy-sol1.ckcmolkh760v.us-east-1.rds.amazonaws.com", "root", "root12345", "piratefy");
 
     if(mysqli_connect_errno()){
         echo "Falha ao conectar: " . mysqli_connect_errno();
